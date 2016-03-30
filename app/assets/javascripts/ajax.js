b036b5217888f1ea8a53ce6facfa61e3276206e8
Tom Ulman <Tom@Toms-MacBook-Pro.local>
2016-03-30 14:41:28 +1100

    working on implementing ajax for highscores but midding highscores controller

---
+++ app/assets/javascripts/ajax.js
@@ -0,0 +1,33 @@
+//
+// var storeScore = function(score) {
+//
+// }
+// var app = app || {}
+//
+//
+//
+//     $.ajax({
+//       url: "/highscores",
+//       method: "POST",
+//       dataType: "JSON",
+//       data: {
+//         highscore: {
+//           score: app.score,
+//           user_id: user_id
+//         }
+//       },
+//       success:function(data){
+//         console.log(data);
+//       }
+//     })
+//   })
+//
+//
+
+
+/
+// class PagesController < ApplicationController
+//   def show
+//     @scores = Score.order(score: :desc).limit(5)
+//   end
+// end
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
 //= require phaser.min
 //= require create
 //= require update
